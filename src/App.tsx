b0688cbfa68894f792c6508ae3f730a485b63f23
TSKeerthiga <keerthiga.thurvassurendran@ideas2it.com>
2025-05-30 16:27:27 +0530

    tailwind is using 3.4.17 version because 4+ was not applied sytled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'; // Only if you need custom styles, not Tailwind directives
+import './index.css';  // This should include Tailwind directives
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
       <h1 className="text-3xl font-bold text-red-500">Hello Tailwind!</h1>
+      <div className='text-red-500'>Sample text</div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <h1 className="bg-green-200 text-3xl text-red-500 p-4">Hello Tailwind!</h1>
+        <h1 className="bg-green-200 text-3xl text-blue-500 p-4">Hello Tailwind!</h1>
 
         <App />
     </React.StrictMode>
